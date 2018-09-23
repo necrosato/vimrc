ef719fd62e94fd7e7d1ebd7f2bca40246f7382b6
Naookie Sato <naookie@revl.com>
2018-09-23 08:30:50 -0700

    vimrc with stuff from oliver

---
+++ revl/.vim/templates/template_.cpp
@@ -0,0 +1,11 @@
+//
+// <+VIM_FILE,:t+>
+// Revl
+//
+// Created by Naookie Sato on <+DATE,%m/%d/%Y+>
+// Copyright © 2018 Revl. All rights reserved.
+//
+
+namespace XXX {
+
+}  // namespace
---
+++ revl/.vim/templates/template_.h
@@ -0,0 +1,16 @@
+//
+// <+VIM_FILE,:t+>
+// Revl
+//
+// Created by Naookie Sato on <+DATE,%m/%d/%Y+>
+// Copyright © 2018 Revl. All rights reserved.
+//
+
+#ifndef <+INCLUDE_GUARD,.*cpp+>
+#define <+INCLUDE_GUARD,.*cpp+>
+
+namespace XXX {
+
+}  // namespace
+
+#endif  // <+INCLUDE_GUARD,.*cpp+>
