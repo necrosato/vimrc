089cb7d57c68b97634467d968e8ee081791dba49
Naookie Sato <necrosato@live.com>
2018-11-13 16:47:28 -0800

    more

---
+++ necrosato/.vim/templates/template_.h
@@ -0,0 +1,16 @@
+//
+// <+VIM_FILE,:t+>
+// Naookie Sato
+//
+// Created by Naookie Sato on <+DATE,%m/%d/%Y+>
+// Copyright © 2018 Naookie Sato. All rights reserved.
+//
+
+#ifndef <+INCLUDE_GUARD,.*cpp+>
+#define <+INCLUDE_GUARD,.*cpp+>
+
+namespace XXX {
+
+}  // namespace
+
+#endif  // <+INCLUDE_GUARD,.*cpp+>
---
+++ necrosato/.vim/templates/template_.ino
@@ -0,0 +1,7 @@
+//
+// <+VIM_FILE,:t+>
+// Naookie Sato
+//
+// Created by Naookie Sato on <+DATE,%m/%d/%Y+>
+// Copyright © 2018 Naookie Sato. All rights reserved.
+//
